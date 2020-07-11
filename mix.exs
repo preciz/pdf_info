117d44b40252c0338673bce248a64eb31be70294
preciz <barna.kovacs.codes@gmail.com>
2020-07-11 12:32:00 +0200

    Prepare release

--- mix.exs
+++ mix.exs
@@ -1,28 +1,50 @@
-defmodule PdfInfo.MixProject do
+defmodule PDFInfo.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @github "https://github.com/preciz/pdf_info"
 
   def project do
     [
       app: :pdf_info,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: @version,
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: "Extracts all /Info and /Metadata from a PDF binary using Regex.",
+
+      # Docs
+      name: "PDFInfo",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Barna Kovacs"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "PDFInfo",
+      source_ref: "v#{@version}",
+      source_url: @github
     ]
   end
 end
