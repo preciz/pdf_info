e254a2122fb87d9c42100df147acb1f411170b8d
preciz <barna.kovacs.codes@gmail.com>
2020-07-11 12:44:31 +0200

    Use ex_doc 0.22 dependency

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule PDFInfo.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 
